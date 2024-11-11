b3401d6acdce2bb846b4607d7df506de03d533dc
Oeshik0 <meghr714@gmail.com>
2024-11-11 20:00:44 +0600

    oeshik

---
+++ src/Shop/SingleBook.jsx
@@ -0,0 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const SingleBook = () => {
+    const {_id , bookTitle, imageURL} = useLoaderData();
+    return (
+        <div className="px-4 mt-28 lg:px-24">
+            <img src={imageURL} alt="" className="h-96"/>
+            <h2>{bookTitle}</h2>
+            
+        </div>
+    );
+};
+
+export default SingleBook;
